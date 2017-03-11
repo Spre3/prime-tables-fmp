49e62e12df36db7d985ec7e8f6518357d2178ee9
Bert <robertocpsilva@gmail.com>
2017-03-11 18:10:26 +0000

    Added logic for calculating first N primes
    * Created a PrimesTables class with getPrimes and isPrime functions

--- prime-tables.js
+++ prime-tables.js
@@ -1,1 +1,23 @@
-﻿
+﻿var PrimeTables = function () {
+};
+
+PrimeTables.prototype.getPrimes = function (numberOfPrimes) {
+    if (numberOfPrimes < 1) return null;
+
+    var primes = [2];
+    var number = 3;
+    while (primes.length < numberOfPrimes) {
+        if (this.isPrime(number))
+            primes.push(number);
+        number++;
+    }
+    return primes;
+};
+
+PrimeTables.prototype.isPrime = function(number) {
+    for (var i = 2; i < number; i++) {
+        if (number % i === 0)
+            return false;
+    }
+    return true;
+};
